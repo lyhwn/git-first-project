44e8fa6e096a49aece5286a1dc3536cb1f6bfc00
hrh <1040542203@qq.com>
2020-11-29 13:27:24 +0800

    添加test方法

---
+++ src/com/itheima/git/FirstClass.java
@@ -0,0 +1,15 @@
+package com.itheima.git;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+    }
+
+    public void test(){
+        System.out.println("test");
+    }
+}
